13bda1b42e7f21489dba23c0c3a4730c910c3577
Your Name <deshmukhanjali167@gmail.com>
2024-10-21 15:56:09 +0530

    mobileapp

---
+++ src/app/component/firstpage/firstpage.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-firstpage',
+  standalone: true,
+  imports: [RouterLink
+  ],
+  templateUrl: './firstpage.component.html',
+  styleUrl: './firstpage.component.css'
+})
+export class FirstpageComponent {
+
+}
